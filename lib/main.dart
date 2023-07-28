d279999598920e3b1127f16706c4db9d6fe630bb
Haier <faheemrao449@gmail.com>
2023-07-28 19:15:41 +0500

    with Search function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:ticketbookingapp/Screens/profile.dart';
 import 'package:ticketbookingapp/Screens/search.dart';
 import 'package:ticketbookingapp/Screens/ticket.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
 
         debugShowCheckedModeBanner: false,
         title: 'Ticket Booking App',
